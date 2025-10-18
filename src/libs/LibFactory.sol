d318aee7404b4dd460caa6198ba86af548153c67
David Dada <daveproxy80@gmail.com>
2025-10-18 17:07:54 +0100

    Update LibFactory

--- src/libs/LibFactory.sol
+++ src/libs/LibFactory.sol
@@ -1,16 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+import {DiamondFactory} from "../DiamondFactory.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
-import {LibDiamond} from "@diamond/libraries/LibDiamond.sol";
-import {IFactory} from "../interfaces/IFactory.sol";
-import {IInitializer} from "../interfaces/IInitializer.sol";
 
 library LibFactory {
     using Clones for address;
 
     function _create(address _owner) internal returns (address diamond_) {
         diamond_ = address(this).clone();
-        IInitializer(diamond_).initialize(_owner);
+        DiamondFactory(payable(diamond_)).initialize(address(this), _owner);
     }
 }
